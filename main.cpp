afbd93b9631bbf45ac3cb18e92b36bbf442251be
stephanie2102 <93931535+stephanie2102@users.noreply.github.com>
2022-06-15 20:26:41 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+/*	
+	int i = 1;
+	do{
+		cout<<i<<endl;
+		i++;
+		
+	}while(i <= 10);
+	
+
+ /*int cont = 1;
+ while(cont <=10){
+ 	cout<<cont<<endl;
+ 	cont++;
+ }
+    cout<<"wiii se acabo";
+}
+*/
+/*int contador = 1;
+whhile(contador <= 20){
+	cout <<"debo hacer la tarea"<<" "<<contador<<endl;
+	contaddor++;
+	
+}
+cout <<"wii se acabo"
+for afecta una sola linea de codigo
+*/
+/*for (int i=1; i<=10; i++)
+cout<<i<<enddl;
+*/
+//menu
+int selection, 1;
+do(
+ cout<<"\n menu 1, 2, 3 otro opcion es alir \n ";
+ cout<<"1; opcion";
+ cout<<"2: opcion";
+ cout<<"3: opcion";
+ cout<<"otra opcion";
+ cout<<"ingresa la selccion";
+ switch(seleccion){
+ 	
+ case 1;
+ cout<<"opcion 1";
+ break;
+ case2;
+ break;
+ cout<<"opcion 2" ;
+ break;
+ case3
+ cout<<"opcion 3";
+ }
+)while((selecion> 0)yy(seleccion <4));
+}//investigar los componentes de elementos de una lista en c++
